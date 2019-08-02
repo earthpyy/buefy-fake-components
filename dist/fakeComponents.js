143d3ee63cebd79d607f74ee0ec488f64bf7eb83
Preeti Yuankrathok <preetisatit@gmail.com>
2019-08-03 02:43:25 +0700

    support Buefy 0.8.1

--- dist/fakeComponents.js
+++ dist/fakeComponents.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 
 import Autocomplete from 'buefy/src/components/autocomplete/Autocomplete'
+import Button from 'buefy/src/components/button/Button'
 import Checkbox from 'buefy/src/components/checkbox/Checkbox'
 import CheckboxButton from 'buefy/src/components/checkbox/CheckboxButton'
+import Clockpicker from 'buefy/src/components/clockpicker/Clockpicker'
+import ClockpickerFace from 'buefy/src/components/clockpicker/ClockpickerFace'
 import Collapse from 'buefy/src/components/collapse/Collapse'
 import Datepicker from 'buefy/src/components/datepicker/Datepicker'
+import DatepickerMonth from 'buefy/src/components/datepicker/DatepickerMonth'
 import DatepickerTable from 'buefy/src/components/datepicker/DatepickerTable'
 import DatepickerTableRow from 'buefy/src/components/datepicker/DatepickerTableRow'
 import Dialog from 'buefy/src/components/dialog/Dialog'
@@ -15,14 +19,28 @@ import FieldBody from 'buefy/src/components/field/FieldBody'
 import Icon from 'buefy/src/components/icon/Icon'
 import Input from 'buefy/src/components/input/Input'
 import Loading from 'buefy/src/components/loading/Loading'
+import Menu from 'buefy/src/components/menu/Menu'
+import MenuItem from 'buefy/src/components/menu/MenuItem'
+import MenuList from 'buefy/src/components/menu/MenuList'
 import Message from 'buefy/src/components/message/Message'
 import Modal from 'buefy/src/components/modal/Modal'
+import Navbar from 'buefy/src/components/navbar/Navbar'
+import NavbarBurger from 'buefy/src/components/navbar/NavbarBurger'
+import NavbarItem from 'buefy/src/components/navbar/NavbarItem'
 import Notification from 'buefy/src/components/notification/Notification'
+import NotificationNotice from 'buefy/src/components/notification/NotificationNotice'
+import Numberinput from 'buefy/src/components/numberinput/Numberinput'
 import Pagination from 'buefy/src/components/pagination/Pagination'
+import Progress from 'buefy/src/components/progress/Progress'
 import Radio from 'buefy/src/components/radio/Radio'
 import RadioButton from 'buefy/src/components/radio/RadioButton'
 import Select from 'buefy/src/components/select/Select'
+import Slider from 'buefy/src/components/slider/Slider'
+import SliderThumb from 'buefy/src/components/slider/SliderThumb'
+import SliderTick from 'buefy/src/components/slider/SliderTick'
 import Snackbar from 'buefy/src/components/snackbar/Snackbar'
+import StepItem from 'buefy/src/components/steps/StepItem'
+import Steps from 'buefy/src/components/steps/Steps'
 import Switch from 'buefy/src/components/switch/Switch'
 import Table from 'buefy/src/components/table/Table'
 import TableColumn from 'buefy/src/components/table/TableColumn'
@@ -38,10 +56,14 @@ import Tooltip from 'buefy/src/components/tooltip/Tooltip'
 import Upload from 'buefy/src/components/upload/Upload'
 
 Vue.component('b-autocomplete', Autocomplete)
+Vue.component('b-button', Button)
 Vue.component('b-checkbox', Checkbox)
 Vue.component('b-checkbox-button', CheckboxButton)
+Vue.component('b-clockpicker', Clockpicker)
+Vue.component('b-clockpicker-face', ClockpickerFace)
 Vue.component('b-collapse', Collapse)
 Vue.component('b-datepicker', Datepicker)
+Vue.component('b-datepicker-month', DatepickerMonth)
 Vue.component('b-datepicker-table', DatepickerTable)
 Vue.component('b-datepicker-table-row', DatepickerTableRow)
 Vue.component('b-dialog', Dialog)
@@ -52,14 +74,28 @@ Vue.component('b-field-body', FieldBody)
 Vue.component('b-icon', Icon)
 Vue.component('b-input', Input)
 Vue.component('b-loading', Loading)
+Vue.component('b-menu', Menu)
+Vue.component('b-menu-item', MenuItem)
+Vue.component('b-menu-list', MenuList)
 Vue.component('b-message', Message)
 Vue.component('b-modal', Modal)
+Vue.component('b-navbar', Navbar)
+Vue.component('b-navbar-burger', NavbarBurger)
+Vue.component('b-navbar-item', NavbarItem)
 Vue.component('b-notification', Notification)
+Vue.component('b-notification-notice', NotificationNotice)
+Vue.component('b-numberinput', Numberinput)
 Vue.component('b-pagination', Pagination)
+Vue.component('b-progress', Progress)
 Vue.component('b-radio', Radio)
 Vue.component('b-radio-button', RadioButton)
 Vue.component('b-select', Select)
+Vue.component('b-slider', Slider)
+Vue.component('b-slider-thumb', SliderThumb)
+Vue.component('b-slider-tick', SliderTick)
 Vue.component('b-snackbar', Snackbar)
+Vue.component('b-step-item', StepItem)
+Vue.component('b-steps', Steps)
 Vue.component('b-switch', Switch)
 Vue.component('b-table', Table)
 Vue.component('b-table-column', TableColumn)
